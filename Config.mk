4539dd98f777d64f47d280bee0e2a3e042bcd6c3
takaswat <takaswat@actapio.com>
2021-03-16 08:32:58 -0700

    fix typo

--- Config.mk
+++ Config.mk
@@ -1,7 +1,7 @@
 #!/usr/bin/make -f
 
 GO               := go
-DOCKER_WORKDIR   := /go/src/github.com/actapio/moxspec-metrics-agent
+DOCKER_WORKDIR   := /go/src/github.com/moxspec/moxspec-metrics-agent
 DOCKER_RUN       := sudo docker run --rm -v $(CURDIR):$(DOCKER_WORKDIR) --workdir=$(DOCKER_WORKDIR)
 CENTOS_CONTAINER := takaswat/moxspec-centos:7
 BINNAME          := mox-metrics-agent
