9a7c4de88a889af05f7f076f905a1d39c7a48146
Takashi Watanabe <44848317+takaswat@users.noreply.github.com>
2021-05-13 13:44:28 -0700

    fix the docker image (#5)

--- Config.mk
+++ Config.mk
@@ -3,5 +3,5 @@
 GO               := go
 DOCKER_WORKDIR   := /go/src/github.com/moxspec/moxspec-metrics-agent
 DOCKER_RUN       := sudo docker run --rm -v $(CURDIR):$(DOCKER_WORKDIR) --workdir=$(DOCKER_WORKDIR)
-CENTOS_CONTAINER := takaswat/moxspec-centos:7
+CENTOS_CONTAINER := actapio/moxspec-centos:7
 BINNAME          := mox-metrics-agent
